a81a916284263960b0182e6dbfa179388a7eb387
jean Pierre <jean.pierre988@hotmail.com>
2021-09-07 09:48:59 -0300

    Added Cache to TopicosController, lista method

--- src/main/java/br/com/jean/forum/controller/TopicosController.java
+++ src/main/java/br/com/jean/forum/controller/TopicosController.java
@@ -8,10 +8,12 @@ import br.com.jean.forum.modelo.Topico;
 import br.com.jean.forum.repository.CursoRepository;
 import br.com.jean.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -32,8 +34,9 @@ public class TopicosController {
     CursoRepository cursoRepository;
 
     @GetMapping
-    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao){
-        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordenacao);
+    @Cacheable(value = "listaDeTopicos")
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+                                 @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao){
 
         if (nomeCurso != null) {
             Page<Topico> topicoList = topicoRepository.findByCurso_nome(nomeCurso, paginacao);
