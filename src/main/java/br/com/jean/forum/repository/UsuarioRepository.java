46a2d714bc166963a9f207581d4c97473fced831
jean Pierre <jean.pierre988@hotmail.com>
2021-09-07 22:43:51 -0300

    Implementing Authentication

---
+++ src/main/java/br/com/jean/forum/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.jean.forum.repository;
+
+import br.com.jean.forum.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Qmmbi.MkInnnsl0PS.aLIORjm6lEGQ6w8Jcgxk2HT6usc/wF45sUq');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
