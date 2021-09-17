ce1e279612a0ab49d0b507d831e38725ed4ec374
jean Pierre <jean.pierre988@hotmail.com>
2021-09-16 22:56:19 -0300

    *Using TOKEN to validated user *Using Spring-boot-Admin

---
+++ src/main/java/br/com/jean/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,53 @@
+package br.com.jean.forum.config.security;
+
+import br.com.jean.forum.modelo.Usuario;
+import br.com.jean.forum.repository.UsuarioRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+
+    private UsuarioRepository usuarioRepository;
+
+    public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository userRespository) {
+        this.tokenService = tokenService;
+        this.usuarioRepository = userRespository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recuperarToken(request);
+        boolean valido = tokenService.isTokenValido(token);
+
+        if (valido) {
+            autenticarCliente(token);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void autenticarCliente(String token) {
+        Long idUser = tokenService.getIdUser(token);
+        Usuario user = usuarioRepository.findById(idUser).get();
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if(token == null || token.isEmpty() || !token.startsWith("Bearer")){
+            return null;
+        }
+        return token.substring(7, token.length());
+    }
+}
--- src/main/java/br/com/jean/forum/config/security/SecutiryConfigurations.java
+++ src/main/java/br/com/jean/forum/config/security/SecutiryConfigurations.java
@@ -1,14 +1,20 @@
 package br.com.jean.forum.config.security;
 
+import antlr.Token;
+import br.com.jean.forum.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -16,6 +22,18 @@ public class SecutiryConfigurations extends WebSecurityConfigurerAdapter {
 
     @Autowired
     AutenticacaoService autenticacaoService;
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -27,8 +45,12 @@ public class SecutiryConfigurations extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin();
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
